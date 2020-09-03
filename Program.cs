e09e174d2ee0c27aebfed7c0e882f4dfb0f6c94b
Dmitry <55185673+Va1halla@users.noreply.github.com>
2020-09-04 02:23:01 +0300

    d.gavrilov
    Remarks

--- Program.cs
+++ Program.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using System.Text;
 using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Threading;
-using System.Linq.Expressions;
-using System.Xml;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System.Net;
+using System.Reflection;
 
 namespace CurrencyCourses
 {
@@ -13,44 +12,82 @@ namespace CurrencyCourses
     {
         static void Main(string[] args)
         {
-            DateTime date = new DateTime();
-            date = DateTime.Today;
+            Dictionary<string, object> dict = JsonConvert.DeserializeObject<Dictionary<string, object>>(Core.APIResponse());
+            foreach(KeyValuePair<string,object> kvp in dict)
+            {
 
-            XMLParser Answer = new XMLParser();
-            Console.WriteLine(Answer.Request("28", "08", "2020"));
+            }
 
+            //Dictionary<string, Dictionary<string, double>> FullDict = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, double>>>();
         }
+        
     }
 
-    class XMLParser
+    public class Core
     {
-        public string Request(string dd, string mm, string yyyy)
+        public static string APIResponse() //получение json из API ExchangeRates
         {
-            string Url = "http://www.cbr.ru/scripts/XML_daily.asp?date_req=" + dd + "/" + mm + "/" + yyyy;
-            Console.WriteLine(Url);
+            string Url = "https://api.exchangeratesapi.io/latest?base=USD";
             var WebClient = new WebClient();
             string Response = WebClient.DownloadString(Url);
-            //Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-            //Encoding cp1251 = Encoding.GetEncoding(1251);
-            //Encoding utf8 = Encoding.UTF8;
-            //byte[] cpbytes = cp1251.GetBytes(WebClient.DownloadString(Url));
-            //byte[] utfbytes = Encoding.Convert(cp1251, utf8, cpbytes);
-
-            //char[] utfchars = new char[utf8.GetCharCount(utfbytes, 0, utfbytes.Length)];
-            //utf8.GetChars(utfbytes, 0, utfbytes.Length, utfchars, 0);
-            //string Response = new string(utfchars);
-            //Console.WriteLine(Response);
             return Response;
         }
+        public static void Deserialize()
+        {
+            Root root = JsonConvert.DeserializeObject<Root>(APIResponse());
+        }
+        public static void MainOutput(string Valute)
+        {
 
-        //public string RequestXML(string dd, string mm, string yyyy)
-        //{
-        //    string Url = "http://www.cbr.ru/scripts/XML_daily.asp?date_req=" + dd + "/" + mm + "/" + yyyy;
-        //    var WebClient = new WebClient();
-        //    XmlDocument source = new XmlDocument();
-        //    source.LoadXml(WebClient.DownloadString(Url));
-        //    Console.WriteLine(source);
-        //    return null;
-        //}
+            DateTime dateTime = DateTime.Now;
+            Console.WriteLine($"Привет, я занимаюсь показом курсов валют, относительно доллара США.\nCегодня {dateTime.ToString()}");
+            Console.WriteLine("Для того чтобы начать работать, необходимо указать валюту, курс которой вам необходимо узнать.\nСписок доступных валют:");
+        }
     }
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+    public class Rates
+    {
+        public double CAD { get; set; }
+        public double HKD { get; set; }
+        public double ISK { get; set; }
+        public double PHP { get; set; }
+        public double DKK { get; set; }
+        public double HUF { get; set; }
+        public double CZK { get; set; }
+        public double GBP { get; set; }
+        public double RON { get; set; }
+        public double SEK { get; set; }
+        public double IDR { get; set; }
+        public double INR { get; set; }
+        public double BRL { get; set; }
+        public double RUB { get; set; }
+        public double HRK { get; set; }
+        public double JPY { get; set; }
+        public double THB { get; set; }
+        public double CHF { get; set; }
+        public double EUR { get; set; }
+        public double MYR { get; set; }
+        public double BGN { get; set; }
+        public double TRY { get; set; }
+        public double CNY { get; set; }
+        public double NOK { get; set; }
+        public double NZD { get; set; }
+        public double ZAR { get; set; }
+        public double USD { get; set; }
+        public double MXN { get; set; }
+        public double SGD { get; set; }
+        public double AUD { get; set; }
+        public double ILS { get; set; }
+        public double KRW { get; set; }
+        public double PLN { get; set; }
+    }
+
+    public class Root
+    {
+        public Rates Rates { get; set; }
+        public string Base { get; set; }
+        public string date { get; set; }
+
+    }
+
 }
