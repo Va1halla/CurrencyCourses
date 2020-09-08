81ac9e1c5211f5cc1304f298942bc187259a251b
Dmitry <55185673+Va1halla@users.noreply.github.com>
2020-09-09 02:00:15 +0300

    Переписал немного по стандартам(нет) ООП

--- Program.cs
+++ Program.cs
@@ -3,76 +3,126 @@ using System.Text;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using System.Net;
-using System.Reflection;
 
 namespace CurrencyCourses
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Core.MainOutput();
             Console.ReadKey();
         }
         
     }
-
     public class Core
     {
-        public static string APIResponse() //получение json из API ExchangeRates
+        public class Root
         {
-            string Url = "https://api.exchangeratesapi.io/latest?base=USD";
-            var WebClient = new WebClient();
-            string Response = WebClient.DownloadString(Url);
-            return Response;
+            public Dictionary<string, double> Rates = new Dictionary<string, double>();
+            public string Base { get; set; }
+            public string date { get; set; }
+
+        }
+        public class Deserializer
+        {
+            public Deserializer() { defaultValute = ""; defaultValuteIsEnabled = false; }
+            public Deserializer(string defaultValute) { this.defaultValute = defaultValute; defaultValuteIsEnabled = true; }
+            bool defaultValuteIsEnabled;
+            string defaultValute;
+            public Dictionary<string, double> mainDictionary(string defaultValute,bool defaultValuteIsEnabled)
+            {
+                if (defaultValuteIsEnabled)
+                {
+                    Root Valuelist = new Root();
+                    string json = APIResponse(defaultValute);
+                    Valuelist = JsonConvert.DeserializeObject<Root>(json);
+                    return Valuelist.Rates;
+                }
+                else
+                {
+                    Root Valuelist = new Root();
+                    string json = APIResponse();
+                    Valuelist = JsonConvert.DeserializeObject<Root>(json);
+                    return Valuelist.Rates;
+                }
+              
+            }
+            //полный буллщит, но по другому я не сообразил xD
+            public Dictionary<string, double> Returner()
+            {
+                return mainDictionary(defaultValute, defaultValuteIsEnabled);
+            }
+            private static string APIResponse(string defaultValute) //получение json из API ExchangeRates для получения курсов валют относительно выбранной валюты
+            {
+                string Url = "https://api.exchangeratesapi.io/latest?base=" + defaultValute;
+                var WebClient = new WebClient();
+                string Response = WebClient.DownloadString(Url);
+                return Response;
+            }
+            private static string APIResponse() //получение json из API ExchangeRates для получения списка валют
+            {
+                string Url = "https://api.exchangeratesapi.io/latest";
+                var WebClient = new WebClient();
+                string Response = WebClient.DownloadString(Url);
+                return Response;
+            }
         }
 
-        public static void MainOutput()//Десериализация и вывод
+
+        public static void MainOutput()//вывод
         {
-            Root dict = new Root();
-            string json = Core.APIResponse();
-            dict = JsonConvert.DeserializeObject<Root>(json);
+            
             //Вывод шапки
             DateTime dateTime = DateTime.Now;
-            Console.WriteLine($"Привет, я занимаюсь показом курсов валют, относительно доллара США.\nCегодня {dateTime.ToString()}");
-            Console.WriteLine("Для того чтобы начать работать, необходимо указать валюту, курс которой вам необходимо узнать.\nСписок доступных валют:");
-            foreach (KeyValuePair<string, string> kvp in dict.Rates)
+            Console.WriteLine($"Привет, я занимаюсь показом курсов валют, относительно выбранной валюты.\nCегодня {dateTime.ToString()}");
+            Console.WriteLine("Для того чтобы начать работать, необходимо указать дефолтную валюту и валюту, которую необходимо узнать.\nСписок доступных валют:");
+            Deserializer list = new Deserializer();
+            Dictionary<string, double> Valuelist = list.Returner();
+
+            foreach (KeyValuePair<string, double> kvp in Valuelist)
             {
                 Console.Write($"{kvp.Key}\t");
             }
-            while (true) 
-            {
-                Console.WriteLine("\nВведите необходимую валюту, или нажмите Enter, чтобы получить список всех актуальных курсов.");
-                string valute = Console.ReadLine().ToUpper();
-                if (string.IsNullOrWhiteSpace(valute))
+            // Установка дефолтной валюты
+            while (true) {
+                Console.Write("\nДефолтная валюта: ");
+                string defaultValute = Console.ReadLine().ToUpper();
+                if (string.IsNullOrWhiteSpace(defaultValute))
                 {
-                    foreach (KeyValuePair<string, string> kvp in dict.Rates)
-                    {
-                        Console.WriteLine($"{kvp.Key}\t{kvp.Value}");
-                    }
-                    break;
-                }
-                //Поиск непосредственно по ключу
-                try
-                {
-                    Console.WriteLine($"{valute} value = {dict.Rates[valute]}.");
-                    break;
-                }
-                catch (KeyNotFoundException)
-                {
-                    Console.WriteLine($"Валюта {valute} не найдена в списке.");
+                    Console.WriteLine("Валюта не выбрана.");
                     continue;
                 }
+                else if (Valuelist.ContainsKey(defaultValute) || defaultValute=="EUR")
+                {
+                    Deserializer dict = new Deserializer(defaultValute);
+                    Dictionary<string, double> Courses = dict.Returner();
+                    //выбор валюты для вывода значения
+                    while (true)
+                    {
+                        Console.WriteLine("\nВведите необходимую валюту, или нажмите Enter, чтобы получить список всех актуальных курсов.");
+                        string valute = Console.ReadLine().ToUpper();
+                        if (string.IsNullOrWhiteSpace(valute))
+                        {
+                            //вывод выглядит по уродски(
+                            foreach (KeyValuePair<string, double> kvp in Courses)
+                            {
+                                Console.WriteLine($"\t{kvp.Key} - {Math.Round(kvp.Value, 2)}");
+                            }
+                            break;
+                        }
+                        Console.WriteLine($"{valute} value = {Math.Round(Courses[valute],2)}.");
+                        break;
+                    }
+                }
+                else if (!Valuelist.ContainsKey(defaultValute))
+                {
+                    Console.WriteLine("Валюта не найдена.");
+                    continue;
+                }
+                break;
             }
+            
         }
     }
-
-    public class Root
-    {
-        public Dictionary<string, string> Rates = new Dictionary<string, string>();
-        public string Base { get; set; }
-        public string date { get; set; }
-
-    }
-
 }
